05d1225de8e5812c17dde0bf29f244c9d6aa7eae
Scott Schneider <sschneider@puppetlabs.com>
2013-09-30 10:38:13 -0700

    Initial

---
+++ manifests/init.pp
@@ -0,0 +1,51 @@
+class nexus (
+  $source  = 'http://download.sonatype.com/nexus/oss',
+  $dest    = '/var/www',
+  $version = '2.6.2-01'
+) {
+  $source_url = "${source}/nexus-${version}-bundle.tar.gz"
+
+  include apache
+  include java
+
+  apache::port { 'nexus-proxy': port => '80' }
+
+  apache::vhost::proxy { 'nexus-proxy':
+    serveraliases => 'nexus-proxy',
+    port          => 80,
+    dest          => 'http://localhost:8081',
+  }
+
+  file { $dest:
+    ensure  => 'directory',
+    recurse => 'true'
+  }
+
+  exec { 'nexus-download':
+    command => "curl -v --progress-bar -o '/tmp/nexus-${version}-bundle.tar.gz' '${source_url}'",
+    cwd     => '/tmp',
+    path    => [ '/bin', '/usr/bin' ],
+    creates => "/tmp/nexus-${version}-bundle.tar.gz",
+    unless  => "test -d ${dest}/nexus-${version}"
+  }
+
+  exec { 'nexus-extract':
+    command   => "tar -C ${dest} -zxvf /tmp/nexus-${version}-bundle.tar.gz",
+    cwd       => '/tmp',
+    path      => [ '/bin', '/usr/bin' ],
+    creates   => "${dest}/nexus-${version}",
+    subscribe => Exec[ 'nexus-download' ],
+    require   => Exec[ 'nexus-download' ]
+  }
+
+  file { '/etc/init.d/nexus':
+    ensure => 'link',
+    target => "${dest}/nexus-${version}/bin/nexus"
+  }
+
+  service { 'nexus':
+    ensure => 'running',
+    enable => 'true'
+  }
+}
+
